30e4d34da97c036235528dd0b765c9d85aa77766
Michael Le <mle00005@email.cpcc.edu>
2021-02-05 12:59:32 -0500

    Revert "Updated HelloWord.java"
    This reverts commit f11580418a8e8ff07892e0a9b402360addc0bf7b.

--- Helloworld.java
+++ Helloworld.java
@@ -4,10 +4,5 @@ public class Helloworld
     {
       System.out.println("Hello World!");
       
-      //Modifying Program
-      
-      System.out.println("Program modified, but able\n" +
-      "to revert to original version\n" + "using GitHub Desktop.");
-      
     }
 }
